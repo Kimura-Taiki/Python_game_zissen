53927591484b977e58c05fb5635b0b2216ee5abd
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-09-25 02:02:32 +0900

    0621_6:pygame.event.getの処理部分をmod.solve_eventへ移管

--- Chapter6/rf21.py
+++ Chapter6/rf21.py
@@ -1,36 +1,12 @@
 import pygame
-import sys
-from functools import partial
 
-from mod.screen import screen, WIN_SIZE, WIN_Y
+from mod.screen import screen, WIN_Y
+from mod.solve_event import event_mapping, solve_event
 
 # 画像の読み込み
 img_galaxy = pygame.image.load("image_gl/galaxy.png")
 
 bg_y = 0
-
-def quit_event():
-    pygame.quit()
-    sys.exit()
-
-def fullscreen_event(screen, size):
-    screen = pygame.display.set_mode(size=size, flags=pygame.FULLSCREEN)
-
-def windowed_event(screen, size):
-    screen = pygame.display.set_mode(size=size)
-
-event_mapping = [
-    {"type":pygame.QUIT,                            "func":quit_event},
-    {"type":pygame.KEYDOWN, "key":pygame.K_F1,      "func":partial(fullscreen_event, screen=screen, size=WIN_SIZE)},
-    {"type":pygame.KEYDOWN, "key":pygame.K_F2,      "func":partial(windowed_event, screen=screen, size=WIN_SIZE)},
-    {"type":pygame.KEYDOWN, "key":pygame.K_ESCAPE,  "func":partial(windowed_event, screen=screen, size=WIN_SIZE)}]
-
-def solve_event(mapping):
-    for event in pygame.event.get():
-        for map in mapping:
-            if event.type == map["type"] and (event.type != pygame.KEYDOWN or event.key == map["key"]):
-                map["func"]()
-                break
 
 def main(): # メインループ
     global bg_y, screen, event_mapping
