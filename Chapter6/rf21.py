31035a6c0e042d9982699d90a4d35598b2350c18
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-09-25 02:29:07 +0900

    0621_8:BackGroundクラスをモジュール化

--- Chapter6/rf21.py
+++ Chapter6/rf21.py
@@ -2,20 +2,7 @@ import pygame
 
 from mod.screen import screen, WIN_Y
 from mod.solve_event import event_mapping, solve_event
-
-# 背景関連の処理をBackGroundクラスへ集約
-class BackGround():
-    bg_y = 0
-    IMG_GALAXY = pygame.image.load("image_gl/galaxy.png")
-    
-    @classmethod
-    def scroll(cls, speed):
-        BackGround.bg_y = (BackGround.bg_y+speed)%WIN_Y
-
-    @classmethod
-    def draw(cls, screen):
-        screen.blit(BackGround.IMG_GALAXY, [0, BackGround.bg_y-WIN_Y])
-        screen.blit(BackGround.IMG_GALAXY, [0, BackGround.bg_y])
+from mod.background import BackGround
 
 def main(): # メインループ
     global screen, event_mapping
