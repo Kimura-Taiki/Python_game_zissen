e9e24d33c1811c65f3f56238dbff573ccd729b6c
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-09-26 22:36:31 +0900

    0631_3:move_starshipをStarShipクラスへ移管、moveとdrawに分割

--- Chapter6/mod/starship.py
+++ Chapter6/mod/starship.py
@@ -1,7 +1,35 @@
+import pygame
+pygame.init()
+
 class StarShip():
     x = 480
     y = 360
 
+    # 画像の読み込み
+    IMG_SSHIP = pygame.image.load("image_gl/starship.png")
+
     @classmethod
     def coord():
         return StarShip.x, StarShip.y
+    
+    def move(key): # 自機の移動
+        if key[pygame.K_UP] == 1:
+            StarShip.y = StarShip.y - 20
+            if StarShip.y < 80:
+                StarShip.y = 80
+        if key[pygame.K_DOWN] == 1:
+            StarShip.y = StarShip.y + 20
+            if StarShip.y > 640:
+                StarShip.y = 640
+        if key[pygame.K_LEFT] == 1:
+            StarShip.x = StarShip.x - 20
+            if StarShip.x < 40:
+                StarShip.x = 40
+        if key[pygame.K_RIGHT] == 1:
+            StarShip.x = StarShip.x + 20
+            if StarShip.x > 920:
+                StarShip.x = 920
+
+    def draw(screen):
+        screen.blit(StarShip.IMG_SSHIP, [StarShip.x-37, StarShip.y-48])
+
--- Chapter6/rf31.py
+++ Chapter6/rf31.py
@@ -6,28 +6,6 @@ from mod.screen import screen # ウィンドウを作成
 from mod.background import BackGround # 背景を流して描画する命令を提供
 from mod.starship import StarShip # 自機関連のクラスを提供
 
-# 画像の読み込み
-img_sship = pygame.image.load("image_gl/starship.png")
-
-def move_starship(scrn, key): # 自機の移動
-    if key[pygame.K_UP] == 1:
-        StarShip.y = StarShip.y - 20
-        if StarShip.y < 80:
-            StarShip.y = 80
-    if key[pygame.K_DOWN] == 1:
-        StarShip.y = StarShip.y + 20
-        if StarShip.y > 640:
-            StarShip.y = 640
-    if key[pygame.K_LEFT] == 1:
-        StarShip.x = StarShip.x - 20
-        if StarShip.x < 40:
-            StarShip.x = 40
-    if key[pygame.K_RIGHT] == 1:
-        StarShip.x = StarShip.x + 20
-        if StarShip.x > 920:
-            StarShip.x = 920
-    scrn.blit(img_sship, [StarShip.x-37, StarShip.y-48])
-
 
 def main(): # メインループ
     global screen, event_mapping
@@ -42,8 +20,10 @@ def main(): # メインループ
         BackGround.scroll(speed=16)
         BackGround.draw(screen=screen)
 
+        # 自機の移動
         key = pygame.key.get_pressed()
-        move_starship(screen, key)
+        StarShip.move(key=key)
+        StarShip.draw(screen=screen)
 
         # 映像の書き換えと更新周期の設定
         pygame.display.update()
