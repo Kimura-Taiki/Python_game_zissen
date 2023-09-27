397a5891d2fb707ad74e90cb8bf6c49599d03f5e
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-09-27 23:53:48 +0900

    0711_0:敵機と敵弾を表示

--- Chapter7/z.py
+++ Chapter7/z.py
@@ -3,3 +3,9 @@ class Zakki():
         self.z = "Z"
 
 print("z.pyだよ")
+
+# 依存性の注入
+# https://qiita.com/mkgask/items/d984f7f4d94cc39d8e3c
+
+# Injector
+# https://qiita.com/Jazuma/items/9fa15b36f61f9d1e770cjj
