b18dd1231580ff21353d2b6f44f434de38412ef5
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-09-27 11:54:41 +0900

    0651_2:msl系統のx,y,f,noをBulletクラスのインスタンスにして省略

--- Chapter6/mod/bullet.py
+++ Chapter6/mod/bullet.py
@@ -2,34 +2,29 @@ import pygame
 from pygame.locals import K_SPACE
 
 class Bullet():
-    MISSILE_MAX = 200
-    exist = [False]*MISSILE_MAX
-    x = [0]*MISSILE_MAX
-    y = [0]*MISSILE_MAX
-    no = 0
+    bullets = []
 
     IMG_WEAPON = pygame.image.load("image_gl/bullet.png")
 
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
 
     @classmethod
     def set(cls, key, mother): # 自機の発射する弾をセットする
         if key[K_SPACE] == False: return
-        cls.exist[cls.no] = True
-        cls.x[cls.no] = mother.x
-        cls.y[cls.no] = mother.y-50
-        cls.no = (cls.no+1)%cls.MISSILE_MAX
+        cls.bullets.append(Bullet(mother.x, mother.y-50))
 
     @classmethod
     def move(cls): # 弾の移動
-        for i in range(cls.MISSILE_MAX):
-            if cls.exist[i] == False: continue
-            cls.y[i] -= 36
-            if cls.y[i] < 0:
-                cls.exist[i] = False
+        for bullet in cls.bullets:
+            bullet.y -= 36
+            if bullet.y < 0:
+                del bullet
     
     @classmethod
     def draw(cls, screen): # 弾の描画
-        for i in range(cls.MISSILE_MAX):
-            if cls.exist[i] == False: continue
-            screen.blit(cls.IMG_WEAPON, [cls.x[i]-10, cls.y[i]-32])
+        for bullet in cls.bullets:
+            screen.blit(cls.IMG_WEAPON, [bullet.x, bullet.y])
+            
 
