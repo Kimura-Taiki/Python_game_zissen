35069fc414ba25ce9b92d2551a75ded196ab5aa9
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-09-29 01:14:16 +0900

    0711_6:Enemy.moveからfire部分を分離

--- Chapter7/mod/enemy.py
+++ Chapter7/mod/enemy.py
@@ -30,16 +30,23 @@ class Enemy():
     def bring_enemy(cls, tmr): # 敵を出す
         if tmr%30 == 0:
             cls.enemies.append(Enemy(x=randint(20, 940), y=cls.LINE_T, angle=90, type=1, speed=6))
+    
+    def fire(self): # 弾を発射する
+        if self.type == 1 and self.y > 360:
+            self.enemies.append(Enemy(x=self.x, y=self.y, angle=90, type=0, speed=8))
+            self.angle = -45
+            self.speed = 16
 
     @classmethod
     def move(cls): # 敵オブジェクトの移動
         for enemy in cls.enemies:
             enemy.x += enemy.speed*cos(radians(enemy.angle))
             enemy.y += enemy.speed*sin(radians(enemy.angle))
-            if enemy.type == 1 and enemy.y > 360:
-                cls.enemies.append(Enemy(x=enemy.x, y=enemy.y, angle=90, type=0, speed=8))
-                enemy.angle = -45
-                enemy.speed = 16
+            enemy.fire()
+            # if enemy.type == 1 and enemy.y > 360:
+            #     cls.enemies.append(Enemy(x=enemy.x, y=enemy.y, angle=90, type=0, speed=8))
+            #     enemy.angle = -45
+            #     enemy.speed = 16
             if enemy.x < cls.LINE_L or cls.LINE_R < enemy.x or enemy.y < cls.LINE_T or cls.LINE_B < enemy.y:
                 del enemy
 
