d22149cf582e8a6753ba9c72736ec602bfc40986
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-10-10 15:42:19 +0900

    0722_9:EnemyFactoryクラスをEnemyクラスから分離

---
+++ Chapter7/mod/enemy_factory.py
@@ -0,0 +1,52 @@
+import pygame
+pygame.init()
+
+from os.path import dirname
+import sys
+if __name__ == '__main__': sys.path.append(dirname(dirname(__file__)))
+from mod.enemy import Enemy
+
+
+class EnemyFactory():
+    def no_func():
+        pass
+
+    DEFAULT_PARAMS = (('name', None), ('speed', 6), ('angle', 90), ('breakable', True), ('fire', no_func))
+
+    def __init__(self, dict) -> None:
+        self.img = pygame.image.load(dict['img'])
+        for tuple in self.DEFAULT_PARAMS:
+            if tuple[0] in dict.keys():
+                setattr(self, tuple[0], dict[tuple[0]])
+            else:
+                setattr(self, tuple[0], tuple[1])
+    
+    def print(self):
+        print(self.name, self.img, self.speed, self.angle, self.breakable, self.fire)
+
+    def make(self, x, y, dict={}) -> Enemy:
+        enemy = Enemy(x=x, y=y)
+        enemy.img2 = self.img
+        for tuple in self.DEFAULT_PARAMS:
+            setattr(enemy, tuple[0], getattr(self, tuple[0]))
+        for tuple in dict:
+            setattr(enemy, tuple[0], tuple[1])
+        return enemy
+
+torpedo = EnemyFactory({'img':"image_gl/enemy0.png",    'name':"Torpedo",   'speed':10, 'breakable':False})
+
+def torpedo_run(enemy: Enemy): # 弾を発射する、母機の処理にのみ弾の発射機構を追加する
+    if enemy.y > 360:
+        enemy.enemies.append(torpedo.make(x=enemy.x, y=enemy.y))
+        enemy.angle = -45
+        enemy.speed = 16
+
+
+tac = EnemyFactory({'img':"image_gl/enemy1.png",    'name':"Torpedoer", 'fire':torpedo_run})
+tpd = EnemyFactory({'img':"image_gl/enemy0.png",    'name':"Torpedo",   'speed':10, 'breakable':False})
+print(tac, tpd)
+tac.print()
+tpd.print()
+i = tac.make(x=30, y=40)
+print(i, type(i))
+print(i.name, i.img2, i.x, i.y, i.speed, i.angle, i.breakable, i.fire)
--- Chapter7/z.py
+++ Chapter7/z.py
@@ -4,19 +4,23 @@ class Zakki():
     def __init__(self):
         self.hatena = self.chr
 
-class Factory():
-    tpls = (('x', 12),
-            ('y', True))
-    
-    def __init__(self, params={}) -> None:
-        for tpl in self.tpls:
-            if tpl[0] in params.keys():
-                setattr(self, tpl[0], params[tpl[0]])
-            else:
-                setattr(self, tpl[0], tpl[1])
+class Hoge():
+    def __init__(self, name) -> None:
+        self.name = name
 
-f = Factory({'x':38, 'z':"Get"})
-print(f, f.x, f.y)
+    def __del__(self) -> None:
+        print("{}を削除したよ".format(self.name))
+
+h1 = Hoge("Hoge1")
+h2 = Hoge("ほげ２")
+h3 = Hoge("保外三")
+hsa = [h1, h2]
+hsb = [h2, h3]
+del h1
+del h2
+del hsa
+print("削除終わり")
+
 
 # screen.blit(pygame.font.Font(None, size=40).render("", True, (255, 255, 255)), [0, 0])
 
