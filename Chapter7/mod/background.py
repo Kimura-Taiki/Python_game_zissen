c3716ec127f2908cf12817645a5dce064f472f24
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-10-14 15:53:18 +0900

    0722_32:BackGroundクラスに型ヒントを追加

--- Chapter7/mod/background.py
+++ Chapter7/mod/background.py
@@ -6,14 +6,14 @@ from mod.screen import WIN_Y
 
 # 背景関連の処理をBackGroundクラスへ集約
 class BackGround():
-    bg_y = 0
-    IMG_GALAXY = pygame.image.load("image_gl/galaxy.png")
+    bg_y: int = 0
+    IMG_GALAXY: pygame.surface.Surface = pygame.image.load("image_gl/galaxy.png")
     
     @classmethod
-    def scroll(cls, speed):
+    def scroll(cls, speed: int) -> None:
         BackGround.bg_y = (BackGround.bg_y+speed)%WIN_Y
 
     @classmethod
-    def draw(cls, screen):
+    def draw(cls, screen: pygame.surface.Surface) -> None:
         screen.blit(BackGround.IMG_GALAXY, [0, BackGround.bg_y-WIN_Y])
         screen.blit(BackGround.IMG_GALAXY, [0, BackGround.bg_y])
