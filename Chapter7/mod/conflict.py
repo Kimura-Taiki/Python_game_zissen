4bb7a2a5b31e7fcb1a4974f4bf4f213f37def3f6
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-10-14 17:28:09 +0900

    0722_36:Conflictへ型ヒントを追加

--- Chapter7/mod/conflict.py
+++ Chapter7/mod/conflict.py
@@ -9,20 +9,20 @@ from mod.enemy import Enemy
 
 class Conflict():
     @classmethod
-    def hit_bullet_and_enemy(cls, bullets: list[Bullet], enemies: list[Enemy]):
+    def hit_bullet_and_enemy(cls, bullets: list[Bullet], enemies: list[Enemy]) -> None:
         for enemy in enemies:
             if enemy.breakable == False: continue
             hit(enemy, bullets)
 
-def hit(enemy: Enemy, bullets: list[Bullet]) : # 自弾とのヒットチェック
-    w = enemy.img.get_width()
-    h = enemy.img.get_height()
-    r = int((w+h)/4)+12
+def hit(enemy: Enemy, bullets: list[Bullet]) -> None: # 自弾とのヒットチェック
+    w: int = enemy.img.get_width()
+    h: int = enemy.img.get_height()
+    r: int = int((w+h)/4)+12
     for bullet in bullets[:]:
         if get_dis(enemy.x, enemy.y, bullet.x, bullet.y) < r*r:
             bullets.remove(bullet)
             enemy.hldgs.remove(enemy)
             return
 
-def get_dis(x1: int, y1: int, x2: int, y2: int): # 二点間の距離を求める
-    return( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) )
+def get_dis(x1: int, y1: int, x2: int, y2: int) -> int: # 二点間の距離を求める
+    return (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)
