c6cf9a11577d7b41852984e2fea6ec662a13a4a1
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-10-14 15:59:00 +0900

    0722_33:Screenクラスへ型ヒントを追加

--- Chapter7/mod/screen.py
+++ Chapter7/mod/screen.py
@@ -4,17 +4,17 @@ from functools import partial
 
 from mod.solve_event import event_mapping
 
-WIN_X = 960
-WIN_Y = 720
-WIN_SIZE = (WIN_X, WIN_Y)
+WIN_X: int = 960
+WIN_Y: int = 720
+WIN_SIZE: tuple[int, int] = (WIN_X, WIN_Y)
 
 pygame.display.set_caption("Galaxy Lancer")
-screen = pygame.display.set_mode(WIN_SIZE)
+screen: pygame.surface.Surface = pygame.display.set_mode(WIN_SIZE)
 
-def fullscreen_event(screen: pygame.surface.Surface, size: tuple[int, int]):
+def fullscreen_event(screen: pygame.surface.Surface, size: tuple[int, int]) -> None:
     screen = pygame.display.set_mode(size=size, flags=pygame.FULLSCREEN)
 
-def windowed_event(screen: pygame.surface.Surface, size: tuple[int, int]):
+def windowed_event(screen: pygame.surface.Surface, size: tuple[int, int]) -> None:
     screen = pygame.display.set_mode(size=size)
 
 event_mapping.append({"type":pygame.KEYDOWN, "key":pygame.K_F1,      "func":partial(fullscreen_event, screen=screen, size=WIN_SIZE)})
