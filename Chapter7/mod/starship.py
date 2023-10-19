af8d39c0323828e3d24a9139bb4892de10940382
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-10-19 12:38:22 +0900

    0741_13:StarShip本体の描画をGroup.drawで代行

--- Chapter7/mod/starship.py
+++ Chapter7/mod/starship.py
@@ -17,7 +17,7 @@ class StarShip(pygame.sprite.Sprite):
     ]
     WIDTH: int = IMG_SSHIP[0].get_width()
     HEIGHT: int = IMG_SSHIP[0].get_height()
-    GROUP: Any = pygame.sprite.Group()
+    GROUP: pygame.sprite.Group = pygame.sprite.Group()
     print("GROUPは{}です".format(GROUP), type(GROUP))
     V = 20
     KEY_MAPPING = ({"key":K_UP,     "dx": 0, "dy":-V, "roll":0},
@@ -43,8 +43,10 @@ class StarShip(pygame.sprite.Sprite):
             self.roll = map["roll"]
         self.x = min(max(self.x, 40), 920)
         self.y = min(max(self.y, 80), 640)
+        self.rect.center = (self.x, self.y)
 
     def draw(self, screen: pygame.surface.Surface, tmr: int=0, muteki: int=0) -> None:
         if muteki%2 != 0: return
         screen.blit(self.IMG_SSHIP[3], (self.x-8, self.y+40+(tmr%3)*2))
-        screen.blit(source=self.IMG_SSHIP[self.roll], dest=(self.x-37, self.y-48))
+        self.GROUP.draw(screen)
+        # screen.blit(source=self.IMG_SSHIP[self.roll], dest=(self.x-37, self.y-48))
