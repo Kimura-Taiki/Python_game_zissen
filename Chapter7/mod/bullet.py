98366db796c198274000b52541fcc6988cef75b0
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-10-20 22:40:15 +0900

    0741_25:BulletにSpriteを継承し、image,rect,angle属性を追加、修正

--- Chapter7/mod/bullet.py
+++ Chapter7/mod/bullet.py
@@ -7,24 +7,35 @@ from typing import Any
 from os.path import dirname
 import sys
 if __name__ == '__main__': sys.path.append(dirname(dirname(__file__)))
+from mod.sprite import Sprite
 
-class Bullet():
+class Bullet(Sprite):
     IMG_WEAPON = pygame.image.load("image_gl/bullet.png")
 
-    def __init__(self, x: int, y: int, a: int=270, hldgs: Any=None) -> None:
+    def __init__(self, x: int, y: int, angle: int=270, hldgs: Any=None) -> None:
+        super().__init__(group=[], image=self.IMG_WEAPON, cx=x, cy=y)
         self.x: int = x
         self.y: int = y
-        self.a: int = a
+        self.angle: int = angle
+        self.roll_image()
         self.hldgs: list[Bullet] = hldgs
 
+    def roll_image(self) -> None:
+        '''画像をangleに応じて回転'''
+        x = self.rect.centerx
+        y = self.rect.centery
+        self.image = pygame.transform.rotozoom(surface=self.nega, angle=-90-self.angle, scale=1.0)
+        self.rect = self.image.get_rect()
+        self.rect.center = x, y
+ 
     def move(self) -> None:
-        self.x += int(36*cos(radians(self.a)))
-        self.y += int(36*sin(radians(self.a)))
+        self.x += int(36*cos(radians(self.angle)))
+        self.y += int(36*sin(radians(self.angle)))
         if self.y < 0 or self.x < 0 or self.x > 960:
             self.hldgs.remove(self)
 
     def draw(self, screen: pygame.surface.Surface) -> None:
-        img_rz = pygame.transform.rotozoom(surface=self.IMG_WEAPON, angle=-90-self.a, scale=1.0)
+        img_rz = pygame.transform.rotozoom(surface=self.IMG_WEAPON, angle=-90-self.angle, scale=1.0)
         screen.blit(source=img_rz, dest=(self.x-img_rz.get_width()/2, self.y-img_rz.get_height()/2))
 
 key_scp: int = 0
@@ -37,7 +48,7 @@ def bullet_set(key: pygame.key.ScancodeWrapper, bullets: list[Bullet], x: int, y
     key_z = (key_z+1)*key[K_z]
     if key_z == 1 and may_z == True:
         for a in range(160, 390, 10):
-            bullets.append(Bullet(x=x, y=y-50, a=a, hldgs=bullets))
+            bullets.append(Bullet(x=x, y=y-50, angle=a, hldgs=bullets))
         return True
     return False
 
