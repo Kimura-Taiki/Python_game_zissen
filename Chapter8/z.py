e27886f2f42e6c3e9003fec16511868646bd8285
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-11-05 12:44:30 +0900

    0802-21:クロージャーでidxとtmrを変える事で戻り値と処理を削減

--- Chapter8/z.py
+++ Chapter8/z.py
@@ -1,36 +1,19 @@
-# 以下のpythonのコードでCoord.xの代入(=)と加減算(+=,-=)にmax(0, min(100, self._x))のバリデーションが正しく掛かっていますか？
+import functools
 
-class Coord:
-    def __init__(self, x: int, y: int):
-        self._x = x
-        self._y = y
+class MyClass:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-    @property
-    def x(self) -> int:
-        return self._x
+    def add(self, z):
+        return self.x + self.y + z
 
-    @x.setter
-    def x(self, value: int) -> None:
-        '''バリデーションです。xが0から100の間に収まるように、
-        maxとmin関数を噛ませています。
-        ついでにprint命令も噛ませて処理を命じさせています。'''
-        self._x = max(0, min(100, value)); print("Xは{}だよ".format(self._x))
+# インスタンスを作成
+obj = MyClass(10, 20)
 
-    @property
-    def y(self) -> int:
-        return self._y
+# addメソッドを部分適用してz引数を固定
+add_30 = functools.partial(obj.add, z=30)
 
-    @y.setter
-    def y(self, value: int) -> None:
-        self._y = value
-
-# クラスのインスタンスを作成
-coord = Coord(50, 60)
-# x属性に値を代入
-coord.x = 80
-coord.x = 120
-coord.x = 0
-coord.x += 55
-coord.x += 40
-coord.x -= 70
-coord.x -= 9999
+# 部分適用されたメソッドを呼び出す
+result = add_30()
+print(result)  # 出力: 60
