54f7d4a79e297be60ba6a80b878133497bcafcd1
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-11-19 01:14:58 +0900

    J008:ゲーム全体をRacerGameクラスに抽出

--- Chapter10/rf.py
+++ Chapter10/rf.py
@@ -25,59 +25,52 @@ def trapezoid_color(course_point: int) -> pygame.Color:
     return GRAY
 
 
-# class RacerGame():
-#     pygame.init()
-#     pygame.display.set_caption("Python Racer")
-#     screen = pygame.display.set_mode((WX, WY))
-#     '''pygame.surface.Surface : ゲーム画面となるウィンドウです。'''
-#     clock = pygame.time.Clock()
-#     '''pygame.time.Clock : Clock.tick命令用に抱えているClockインスタンスです。'''
-#     IMG_BG = pygame.image.load(PNG_BG).convert()
+class RacerGame():
+    def __init__(self) -> None:
+        pygame.init()
+        pygame.display.set_caption("Python Racer")
+        self.screen = pygame.display.set_mode((WX, WY))
+        '''pygame.surface.Surface : ゲーム画面となるウィンドウです。この面はゲーム内の描画に使用されます。'''
+        self.clock = pygame.time.Clock()
+        '''pygame.time.Clock : ゲームループのフレームレートを制御するためのClockインスタンスです。
+        このクロックは主に `Clock.tick` メソッドを使用して一定のフレームレートを維持します。'''
+        self.IMG_BG = pygame.image.load(PNG_BG).convert()
+        '''pygame.surface.Surface : 背景の複製元となる面です。'''
+        self.CURVE = [5*sin(radians(i-120)) if i > 120 else 0 for i in range(480)]
+        '''コースの該当地点での曲率です。実際に描画する際には視点位置からの曲率積分を使います。'''
+        self.CMAX = len(self.CURVE)
+        '''コースの全長、板の枚数で定義されている。１周するとまた最初から数える。'''
+        self.car_y = 0
+        '''コース上でのスタート地点からの距離を板の枚数で指定しています。'''
 
-#     CURVE = [5*sin(radians(i-120)) if i > 120 else 0 for i in range(480)]
-#     '''コースの該当地点での曲率です。実際に描画する際には視点位置からの曲率積分を使います。'''
-#     CMAX = len(CURVE)
-#     '''コースの全長、板の枚数で定義されている。１周するとまた最初から数える。'''
-#     car_y = 0
-#     '''コース上でのスタート地点からの距離を板の枚数で指定しています。'''
+    def _move_forward(self) -> None:
+        self.car_y = (self.car_y+1) % self.CMAX
 
+    def mainloop(self) -> None:
+        process_input_events(move_forward=self._move_forward)
 
+        di1: float = 0.0
+        board_lx: list[float] = [WX/2-BOARD_W[i]/2+(di1 := di1+self.CURVE[(self.car_y+i) % self.CMAX])/2 for i in range(BOARD)]
+        di2: float = 0.0
+        board_rx: list[float] = [WX/2+BOARD_W[i]/2+(di2 := di2+self.CURVE[(self.car_y+i) % self.CMAX])/2 for i in range(BOARD)]
 
-
-def main() -> None: # メイン処理
-    pygame.init()
-    pygame.display.set_caption("Python Racer")
-    screen = pygame.display.set_mode((WX, WY))
-    clock = pygame.time.Clock()
-    IMG_BG = pygame.image.load(PNG_BG).convert()
-    curve = [5*sin(radians(i-120)) if i > 120 else 0 for i in range(480)]
-    '''コースの該当地点での曲率です。実際に描画する際には視点位置からの曲率積分を使います。'''
-    CMAX = len(curve)
-    '''コースの全長、板の枚数で定義されている。１周するとまた最初から数える。'''
-    car_y = 0
-    '''コース上でのスタート地点からの距離を板の枚数で指定しています。'''
-
-    while True:
-        def move_forward() -> None:
-            nonlocal car_y, CMAX
-            car_y = (car_y+1) % CMAX
-        process_input_events(move_forward=move_forward)
-
-        di: float = 0.0
-        board_lx: list[float] = [WX/2-BOARD_W[i]/2+(di := di+curve[(car_y+i) % CMAX])/2 for i in range(BOARD)]
-        di: float = 0.0
-        board_rx: list[float] = [WX/2+BOARD_W[i]/2+(di := di+curve[(car_y+i) % CMAX])/2 for i in range(BOARD)]
-
-        screen.blit(IMG_BG, [0, 0])
+        self.screen.blit(self.IMG_BG, [0, 0])
 
         # 描画用データをもとに道路を描く
         for i in range(BOARD-1, 0, -1):
-            pygame.draw.polygon(surface=screen, color=trapezoid_color(course_point=car_y+i),
+            pygame.draw.polygon(surface=self.screen, color=trapezoid_color(course_point=self.car_y+i),
                                 points=[[board_lx[i  ], BOARD_BY[i  ]], [board_rx[i  ], BOARD_BY[i  ]],
                                         [board_rx[i-1], BOARD_BY[i-1]], [board_lx[i-1], BOARD_BY[i-1]]])
 
         pygame.display.update()
-        clock.tick(60)
+        self.clock.tick(60)
+
+def main() -> None:
+    '''メイン処理'''
+    game = RacerGame()
+    while True:
+        game.mainloop()
+
 
 if __name__ == '__main__':
     main()
