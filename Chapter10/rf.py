9fbfbf8b8dfadb09f6aee6023a515a12093677cc
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-11-18 16:56:15 +0900

    J006:定数かできる所はmain外部へ排出

--- Chapter10/rf.py
+++ Chapter10/rf.py
@@ -8,7 +8,14 @@ BOARD = 120
 '''描画する板の枚数'''
 WX = 800
 WY = 600
-
+# 道路の板の基本形状を計算
+BOARD_W = [10+(BOARD-i)**2/12 for i in range(BOARD)]
+'''板の横幅です。0が手前、BOARD-1が最遠です。'''
+BOARD_H = [3.4*(BOARD-i)/BOARD for i in range(BOARD)]
+'''板の縦幅です。0が手前、BOARD-1が最遠です。'''
+di: float = 400.0
+BOARD_BY: list[float] = [(di := di+3.4*i/BOARD) for i in range(BOARD)][::-1]
+'''板の描画Y座標です。0が手前、BOARD-1が最遠です。'''
 
 def make_course() -> list[float]:
     '''コースの曲率を作る関数。'''
@@ -41,12 +48,6 @@ def main(): # メイン処理
 
     img_bg = pygame.image.load("image_pr/bg.png").convert()
 
-    # 道路の板の基本形状を計算
-    BOARD_W = [10+(BOARD-i)**2/12 for i in range(BOARD)]
-    '''板の横幅です。0が手前、BOARD-1が最遠です。'''
-    BOARD_H = [3.4*(BOARD-i)/BOARD for i in range(BOARD)]
-    '''板の縦幅です。0が手前、BOARD-1が最遠です。'''
-
     curve = make_course()
     '''コースの該当地点での曲率です。実際に描画する際には視点位置からの曲率積分を使います。'''
     CMAX = len(curve)
@@ -61,22 +62,18 @@ def main(): # メイン処理
             car_y = (car_y+1) % CMAX
         process_input_events(move_forward=move_forward)
 
-
         di: float = 0.0
         board_lx: list[float] = [WX/2-BOARD_W[i]/2+(di := di+curve[(car_y+i) % CMAX])/2 for i in range(BOARD)]
         di: float = 0.0
         board_rx: list[float] = [WX/2+BOARD_W[i]/2+(di := di+curve[(car_y+i) % CMAX])/2 for i in range(BOARD)]
-        di: float = 400.0
-        board_by: list[float] = [(di := di+3.4*i/BOARD) for i in range(BOARD)][::-1]
-
 
         screen.blit(img_bg, [0, 0])
 
         # 描画用データをもとに道路を描く
         for i in range(BOARD-1, 0, -1):
             pygame.draw.polygon(surface=screen, color=trapezoid_color(course_point=car_y+i),
-                                points=[[board_lx[i  ], board_by[i  ]], [board_rx[i  ], board_by[i  ]],
-                                        [board_rx[i-1], board_by[i-1]], [board_lx[i-1], board_by[i-1]]])
+                                points=[[board_lx[i  ], BOARD_BY[i  ]], [board_rx[i  ], BOARD_BY[i  ]],
+                                        [board_rx[i-1], BOARD_BY[i-1]], [board_lx[i-1], BOARD_BY[i-1]]])
 
         pygame.display.update()
         clock.tick(60)
