1ba1d80b24ab66d30479d5d1daf888630bc5e342
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-11-29 19:26:30 +0900

    距離適性もデータに追加したよ

--- レース情報採集/pd.py
+++ レース情報採集/pd.py
@@ -6,8 +6,8 @@ from datetime import datetime
 from collections import Counter
 from functools import reduce, partial
 
-# 1.G1, 2.G2, 3.G3, 4.G, 5.L, 6.OP, 7.４勝, 8.３勝, 9.２勝, 10.１勝, 11.未勝利, 12.新馬戦, 13.勝入
 MAPPING = [r'\d+万', r'未勝利', r'未出走', r'1勝', r'2勝', r'3勝', r'新馬', r'G1', r'G2', r'G3', r'OP', r'\(L\)', r'\(G\)']
+DIACHRONIC = ["Nooo!!!", "G1", "G2", "G3", "(G)", "(L)", "OP", "3勝", "2勝", "1勝"]
 
 # 開催日,開催,天気,R,レース名,映像,距離,頭数,馬場,タイム,ペース,勝ち馬,騎手,調教師,2着馬,3着馬
 
@@ -17,8 +17,10 @@ def load_table(path: str) -> DataFrame:
     skin = [terrain[0] for terrain in df.loc[:, "距離"]]
     distance = [terrain[-(len(terrain)-1):] for terrain in df.loc[:, "距離"]]
     rank = [get_class(name) for name in df.loc[:, "レース名"]]
-    diachronic_class = [get_diachronic_class(name=rank, year=datetime.strptime(date, "%Y/%m/%d").year) for rank, date in zip(rank, df.loc[:, "開催日"])]
-    races["地肌"], races["距離"], races["クラス"], races["通時"] = skin, distance, rank, diachronic_class
+    diachronic_class = [get_diachronic_class(
+        name=rank,year=datetime.strptime(date, "%Y/%m/%d").year) for rank, date in zip(rank, df.loc[:, "開催日"])]
+    group = [get_group(skin, distance) for skin, distance in zip(skin, distance)]
+    races["地肌"], races["距離"], races["クラス"], races["通時"], races["適性"] = skin, distance, rank, diachronic_class, group
     return races
 
 def get_class(name: str) -> str:
@@ -60,9 +62,6 @@ def make_class_count_table() -> None:
     reduce(func, tables).to_csv(path_or_buf="クラス別年間競争一覧.csv", index=False, float_format="%.0f")
     print("クラス別年間競争一覧を作成しました。")
 
-# MAPPING = [r'\d+万', r'未勝利', r'未出走', r'1勝', r'2勝', r'3勝', r'新馬', r'G1', r'G2', r'G3', r'OP', r'\(L\)', r'\(G\)']
-# 1.G1 2.G2 3.G3 4.(G) 5.(L) 6.OP 7.3勝 8.2勝 9.1勝 o.未勝利 z.----
-DIACHRONIC = ["Nooo!!!", "G1", "G2", "G3", "(G)", "(L)", "OP", "3勝", "2勝", "1勝"]
 
 def get_diachronic_class(name: str, year: int=2020) -> str:
     if name[-1] == "万":
@@ -80,10 +79,27 @@ def get_diachronic_class(name: str, year: int=2020) -> str:
         return "o.未勝利"
     return "z.NoClass"
 
-# print(get_diachronic_class("153万"))
-# print(get_diachronic_class("G1"))
+def get_group(skin: str, distance: str) -> str:
+    if skin == "障":
+        return "7.障害"
+    lgh = int(distance)
+    if skin == "芝":
+        if lgh < 1600:
+            return "1.芝 短距離"
+        elif lgh < 1900:
+            return "2.芝 マイル"
+        else:
+            return "3.芝 中以遠"
+    elif skin == "ダ":
+        if lgh < 1600:
+            return "4.砂 短距離"
+        elif lgh < 1900:
+            return "5.砂 マイル"
+        else:
+            return "6.砂 中以遠"
+    return "z.----"
 
 # print(load_table(path="競争一覧/1975/races001.csv"))
+# exit()
 
 make_database()
-# make_class_count_table()
