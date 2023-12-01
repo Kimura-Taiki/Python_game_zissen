ab4165f54b7599613a59b475a9b2d8cf413cee39
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-12-01 15:57:11 +0900

    やっと積み上げ折れ線グラフが書けたよ

--- レース情報採集/z.py
+++ レース情報採集/z.py
@@ -1,63 +1,33 @@
-# import matplotlib.pyplot as plt
-# import japanize_matplotlib
-
-# x = [1, 2, 3, 4, 5]
-# y = [1, 4, 9, 16, 25]
-# plt.plot(x, y, color='limegreen', linewidth=2, linestyle="solid", marker="o")
-# plt.xlabel('X', fontsize=18)
-# plt.ylabel('Y', fontsize=18)
-# plt.grid()
-# plt.annotate('annotation', xy=(3,9), xytext=(1,20), fontsize=15, color="red",
-#              arrowprops=dict(color="grey"))
-# plt.title('グラフタイトル(Graph)')
-# plt.show()
-
-# from pandas_datareader import data
-# import pandas as pd
-# import matplotlib.pyplot as plt
-
-# start = "2019-11-01"
-# end = "2020-11-01"
-
-# df = data.DataReader("^N225", "yahoo", start, end)
-
-# print(df)
-
-import yfinance as yf
-import pandas as pd
 import matplotlib.pyplot as plt
-import japanize_matplotlib
-import matplotlib.dates as mdates
+import pandas as pd
 
-start = "2019-11-01"
-end = "2020-11-01"
+df = pd.read_csv(filepath_or_buffer="通時クラス別年間競争一覧.csv")
 
-df: pd.DataFrame = yf.download("^N225", start=start, end=end)
-# df["index_num"] = df.reset_index().index
-# # print(df.head(20)); exit()
+x = pd.DataFrame(df.columns)
+y = df[(df["通時"] == "o.未勝利") & (df["適性"] == "3.芝 中以遠")].T
+t = list(df.columns)
+# print(x)
+# exit(y)
 
-x = df.index
-y = df["Adj Close"] #終値
+plt.title("Sales by Product (m$)", fontsize = 22) # (3)タイトル
+plt.xlabel("Product", fontsize = 22) # (4)x軸ラベル
+plt.ylabel("Sales", fontsize = 22) # (5)y軸ラベル
+plt.grid(True) # (6)目盛線表示
+plt.tick_params(labelsize=14) # (7)目盛線ラベルサイズ
 
-#移動平均線追加(５日、２５日)
-span05 = 5
-span25 = 25
-df["sma05"] = y.rolling(window=span05).mean()
-df["sma25"] = y.rolling(window=span25).mean()
-
-plt.figure(figsize=(15, 7))
-plt.plot(x, y, label="close", color="skyblue", linewidth=3, linestyle="dashed", marker="o")
-plt.xticks(fontsize=20)
-plt.yticks(fontsize=20)
-plt.xlabel("日付", fontsize=30)
-plt.ylabel("Price", fontsize=30)
-plt.grid(axis="y")
-plt.title("日経平均株価2019-2020", fontsize=35)
-plt.annotate("points", xy=(mdates.date2num(x[10]), y[10]),
-             xytext=(mdates.date2num(x[10])+10, 24000), fontsize=30,
-             color="red", arrowprops=dict(color="black"))
-plt.plot(x, df["sma05"], label="sma05", color="limegreen")
-plt.plot(x, df["sma25"], label="sma25", color="turquoise")
-plt.legend(fontsize=20)
-plt.savefig("N225.png")
+plt.bar(x, y, tick_label=t,
+        align="center", color = "c") # (8)棒グラフ描画
 plt.show()
+
+# df_sales=pd.read_csv("electric_appliances_sales.csv")
+# print(df_sales)
+
+# plt.title("Sales by Product (m$)", fontsize = 22) # (3)タイトル
+# plt.xlabel("Product", fontsize = 22) # (4)x軸ラベル
+# plt.ylabel("Sales", fontsize = 22) # (5)y軸ラベル
+# plt.grid(True) # (6)目盛線表示
+# plt.tick_params(labelsize=14) # (7)目盛線ラベルサイズ
+
+# plt.bar(df_sales["Product"], df_sales["Sales"], tick_label=df_sales["Product"],
+#         align="center", color = "c") # (8)棒グラフ描画
+# plt.show()
---
+++ レース情報採集/z2.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import japanize_matplotlib
+
+# Read the CSV file
+df = pd.read_csv(filepath_or_buffer="通時クラス別年間競争一覧.csv")
+
+# Filter rows for the specified conditions
+filtered_df = df[(df["通時"] == "o.未勝利") & ((df["適性"] == "1.芝 短距離") | (df["適性"] == "2.芝 マイル") | (df["適性"] == "3.芝 中以遠"))]
+
+# Extract x values (years) and y values for each condition
+x = df.columns[2:]
+x = [int(i) for i in list(x)]
+y1 = filtered_df[filtered_df["適性"] == "1.芝 短距離"].values.flatten()[2:]
+y2 = filtered_df[filtered_df["適性"] == "2.芝 マイル"].values.flatten()[2:]
+y3 = filtered_df[filtered_df["適性"] == "3.芝 中以遠"].values.flatten()[2:]
+labels = ["短距離", "マイル", "中以遠"]
+print(x, y1, y2, y3)
+
+fig, ax = plt.subplots()
+ax.stackplot(x, [list(y1), list(y2), list(y3)], labels=labels)
+# ax.stackplot(x, [y1, y2, y3], labels=labels)
+ax.legend(loc="upper left")
+plt.show()
+
+
+exit([len(y1), len(y2), len(y3), [int(i) for i in list(x)]])
+
+# Plot the data
+# plt.plot(x, y1, label="(o.未勝利,1.芝 短距離)")
+# plt.plot(x, y2, label="(o.未勝利,2.芝 マイル)")
+# plt.plot(x, y3, label="(o.未勝利,3.芝 中以遠)")
+
+# Set plot labels and title
+plt.title("Performance Over the Years")
+plt.xlabel("Year")
+plt.ylabel("Performance")
+plt.legend()  # Show legend
+
+# plt.bar(x, y3, tick_label=x, align="center", color = "c") # (8)棒グラフ描画
+# plt.bar(x, y2, bottom=y3, tick_label=x, align="center", color = "b") # (8)棒グラフ描画
+# plt.bar(x, y1, bottom=y3+y2, tick_label=x, align="center", color = "y") # (8)棒グラフ描画
+plt.stackplot(
+  x,
+  y3,
+  y2,
+  y1,)
+
+
+# Show the plot
+plt.show()
