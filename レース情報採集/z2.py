f44cefb96d4f55445d7d30f342ff56cc29ca95d2
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-12-03 23:47:30 +0900

    賞金ゲットー！

--- レース情報採集/z2.py
+++ レース情報採集/z2.py
@@ -1,4 +1,5 @@
 import requests
+from requests import Response
 from bs4 import BeautifulSoup, Tag
 from typing import Any, Type
 from urllib.request import urlopen
@@ -8,30 +9,6 @@ ACCOUNT_LOGIN_ID = 'akari15_5ngo@icloud.com'
 ACCOUNT_PSWD = '1kei24ba8'
 LOGIN_URL = 'https://regist.netkeiba.com/account/?pid=login'
 ARIMA_URL = 'https://db.netkeiba.com/race/198206050809/'
-
-
-# セッションを開始
-session = requests.session()
-
-# ログインが必要な情報
-login_info = {
-    "login_id":ACCOUNT_LOGIN_ID,
-    "pswd":ACCOUNT_PSWD,
-    "pid":"login",
-    "action":"auth",
-    # "return_url2":ARIMA_URL
-}
-
-# action
-res = session.post(LOGIN_URL, data=login_info)
-res.raise_for_status() # エラーならここで例外を発生させる
-arima = session.get(ARIMA_URL)
-with open('logged_in_page.txt', 'wb') as file:
-    file.write(res.content)
-with open('arima_page.txt', 'wb') as file:
-    file.write(arima.content)
-exit("終了！")
-
 
 def enforce_type[X](instance: Any, cond: Type[X]) -> X:
     if isinstance(instance, cond):
@@ -45,6 +22,30 @@ def find_element[X](bsObj: BeautifulSoup, tag: str="div", class_name: str="", ty
     else:
         result = bsObj.find_all(tag, {"class":class_name})[num] if class_name != "" else bsObj.find_all(tag)[num]
     return enforce_type(result, type)
+
+# セッションを開始
+session = requests.session()
+
+# ログインが必要な情報
+login_info = {
+    "login_id":ACCOUNT_LOGIN_ID,
+    "pswd":ACCOUNT_PSWD,
+    "pid":"login",
+    "action":"auth",
+}
+
+# BeautifulSoupインスタンスを取得
+res = session.post(LOGIN_URL, data=login_info)
+res.raise_for_status() # エラーならここで例外を発生させる
+arima = session.get(ARIMA_URL)
+bsObj = BeautifulSoup(arima.content, "html.parser")
+
+# <table>ブロックを取得
+table = find_element(bsObj=bsObj, class_name="race_table_01", tag="table")
+tr5 = find_element(bsObj=table, tag="tr", num=5)
+td13 = find_element(bsObj=tr5, tag="td", num=13)
+prize = float(td13.get_text().strip())*10
+exit(prize)
 
 # セッションを開始してログイン
 with requests.Session() as session:
